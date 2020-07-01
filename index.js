1b69901092b65d7f7ff451debb275aca8c227765
Jenn Mueng <ldrwarlock@gmail.com>
2020-07-01 19:00:20 +0700

    feat: Export a getFuzzyTimezoneFromPoint method

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ module.exports = {
   getFuzzyLocalTimeFromPoint: getFuzzyLocalTimeFromPoint,
   getFuzzyTimezoneFromTile: getFuzzyTimezoneFromTile,
   getFuzzyTimezoneFromQuadkey: getFuzzyTimezoneFromQuadkey,
-  _getParent: _getParent,      // expose for testing
-  _getChildren: _getChildren   // expose for testing
+  getFuzzyTimezoneFromPoint: getFuzzyTimezoneFromPoint,
+  _getParent: _getParent, // expose for testing
+  _getChildren: _getChildren // expose for testing
 };
 
 /**
@@ -28,6 +29,18 @@ function getFuzzyLocalTimeFromPoint(timestamp, point) {
 }
 
 /**
+ * Returns the timezone id at the point of interest.
+ * @param  {Array}   point       a [lng, lat] point of interest
+ * @return {string}              a timezone string object
+ */
+function getFuzzyTimezoneFromPoint(point) {
+  var tile = tilebelt.pointToTile(point[0], point[1], z).join('/');
+  var locale = tiles[tile];
+
+  return locale;
+}
+
+/**
  * Retrieves the timezone of the tile of interest at z8-level accuracy.
  * @param  {Array}  tile   [x, y, z] coordinate of a tile
  * @return {String}        timezone for the tile
@@ -37,22 +50,20 @@ function getFuzzyTimezoneFromTile(tile) {
     var key = tile.join('/');
     if (key in tiles) return tiles[key];
     else throw new Error('tile not found');
-
   } else if (tile[2] > z) {
     // higher zoom level (9, 10, 11, ...)
     key = _getParent(tile).join('/');
     if (key in tiles) return tiles[key];
     else throw new Error('tile not found');
-
   } else {
     // lower zoom level (..., 5, 6, 7)
     var children = _getChildren(tile);
-    var votes = [];  // list of timezone abbrevations
-    var abbrs = {};  // abbrevation to full name lookup table
+    var votes = []; // list of timezone abbrevations
+    var abbrs = {}; // abbrevation to full name lookup table
     children.forEach(function(child) {
       key = child.join('/');
       if (key in tiles) {
-        var tz = tiles[key];   // timezone name
+        var tz = tiles[key]; // timezone name
 
         // Need to use timezone abbreviation becuase e.g. America/Los_Angeles
         // and America/Vancouver are the same. Use a time to determine the
@@ -97,7 +108,7 @@ function _getChildren(tile) {
 
   var children = tilebelt.getChildren(tile);
   return _getChildren(children[0])
-         .concat(_getChildren(children[1]))
-         .concat(_getChildren(children[2]))
-         .concat(_getChildren(children[3]));
+    .concat(_getChildren(children[1]))
+    .concat(_getChildren(children[2]))
+    .concat(_getChildren(children[3]));
 }
